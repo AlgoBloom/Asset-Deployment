9c48900fbcaf56516082534d97286d39c8323c38
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-11-21 16:38:03 -0500

    Started building out submit to network function.

--- assets.js
+++ assets.js
@@ -5,4 +5,7 @@ const algodClient = new algosdk.Algodv2(process.env.ALGOD_TOKEN, process.env.ALG
 const creator = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_CREATOR);
 const receiver = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_RECEIVER);
 
-const 
+const submitToNetwork = async (signedTxn) => {
+    let txn = await algodClient.sendRawTransaction(signedTxn).do();
+    console.log("Transaction :" + txn.txId);
+}
