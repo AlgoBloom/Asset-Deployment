c2dd6a778d246e6908a9beb68813bd45e9c55871
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-11-22 12:42:35 -0500

    Added shell to build out create fungible token function.

--- assets.js
+++ assets.js
@@ -9,4 +9,10 @@ const submitToNetwork = async (signedTxn) => {
     let txn = await algodClient.sendRawTransaction(signedTxn).do();
     console.log("Transaction :" + txn.txId);
     confirmedTxn = await algosdk.waitForConfirmation(algodClient, txn.txId, 4);
-}
+    console.log("Transaction " + txn.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+    return confirmedTxn;
+};
+
+const createFT = async () => {
+
+};
