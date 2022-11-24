9627c171455924692eded0e9920421b804f168fe
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-11-24 16:18:49 -0500

    Configured final board position's NFT.

--- assets.js
+++ assets.js
@@ -47,7 +47,7 @@ const createFT = async () => {
     return confirmedTxn("asset-index");
 };
 
-const createCommunityLeaderNFT1 = async () => {
+const createBoardNFT1 = async () => {
     const from = creator.addr;
     const defaultFrozen = false;
     const unitName = "CL";
@@ -81,7 +81,7 @@ const createCommunityLeaderNFT1 = async () => {
     return confirmedTxn("asset-index");
 };
 
-const createCommunityLeaderNFT2 = async () => {
+const createBoardNFT2 = async () => {
     const from = creator.addr;
     const defaultFrozen = false;
     const unitName = "AM";
@@ -115,7 +115,7 @@ const createCommunityLeaderNFT2 = async () => {
     return confirmedTxn("asset-index");
 };
 
-const createCommunityLeaderNFT3 = async () => {
+const createBoardNFT3 = async () => {
     const from = creator.addr;
     const defaultFrozen = false;
     const unitName = "TR";
@@ -149,7 +149,7 @@ const createCommunityLeaderNFT3 = async () => {
     return confirmedTxn("asset-index");
 };
 
-const createCommunityLeaderNFT4 = async () => {
+const createBoardNFT4 = async () => {
     const from = creator.addr;
     const defaultFrozen = false;
     const unitName = "FE";
@@ -183,7 +183,7 @@ const createCommunityLeaderNFT4 = async () => {
     return confirmedTxn("asset-index");
 };
 
-const createCommunityLeaderNFT1 = async () => {
+const createBoardNFT5 = async () => {
     const from = creator.addr;
     const defaultFrozen = false;
     const unitName = "CL";
